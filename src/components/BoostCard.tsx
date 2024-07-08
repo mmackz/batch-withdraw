5ece9e45ea1ad674d2ddf04a4f81a7129cf82e07
mmackz <matthewmackenz@gmail.com>
2024-07-07 17:17:56 -0700

    feat: add image wildcard

--- src/components/BoostCard.tsx
+++ src/components/BoostCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from '../styles/BoostCard.module.css';
 import { Boost } from '../schemas';
 
@@ -9,6 +10,7 @@ const BoostCard: React.FC<BoostCardProps> = ({ boost }) => {
   return (
     <div className={styles.boostCard}>
       <h4>{boost.name}</h4>
+      <Image src={boost.iconOption} alt={boost.name} width={40} height={40} />
       <p>ID: {boost.id}</p>
       <p>Start: {new Date(boost.boostStart).toLocaleString()}</p>
       <p>End: {new Date(boost.boostEnd).toLocaleString()}</p>
